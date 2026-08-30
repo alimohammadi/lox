8fcd41305d419846a363594365f5015f563568a2
Ali Mohammadi <alimohammadi02321@gmail.com>
2026-08-30 15:10:04 +0330

    Add some comments

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,16 @@
 
 // break the project into four parts
 // Each in their own file
+
+// Discussion: Use of "mod"
+// Coming from python, these "mod" declarations might look similar to "import" statements. 
+// How ever something else is really going on. Instead of importing code, "mod" is declaring
+// the existence of submodule and is required so that the Rust compiler can actually find the code in other files.
+// You only need to specify "mod" in one place (usually main.rs) and nowhere else in the program.
 mod reader;
 mod tokenize;
 mod parser;
 mod evaluate;
-
 
 fn main() {
     println!("Hello, world!");
